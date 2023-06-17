c472eb02e425d9a040bb5be62762152b2f6d8b7f
devdave <devdave@ominian.net>
2023-06-17 11:53:07 -0600

    Reformat and add in ready checks for the bridge API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import { ThemeProvider } from './ThemeProvider'
-import { Book } from './Book'
+import {ThemeProvider} from './ThemeProvider'
+import {Book} from './Book'
+import {useEffect, useState} from "react";
+import {unset} from "lodash";
+import Boundary, {PYWEBVIEWREADY} from "./lib/boundary.ts";
+import {LoadingOverlay} from "@mantine/core";
 
-export default function App () {
-  return (
-    <ThemeProvider>
-      <Book />
-    </ThemeProvider>
-  )
+
+declare global {
+    interface window {
+        pywebview: any;
+    }
+
+}
+
+export default function App() {
+    const [isReady, setIsReady] = useState(false);
+
+    const doReady = () => {
+        setIsReady(true);
+        window.removeEventListener(PYWEBVIEWREADY, doReady);
+    }
+
+    useEffect(() => {
+        //@ts-ignore Fuck the hell off with this window not defined shit
+        if (window.pywebview !== undefined && window.pywebview.api !== undefined) {
+            setIsReady(true);
+        } else {
+            window.addEventListener(PYWEBVIEWREADY, doReady);
+        }
+    });
+
+    if(isReady === false){
+        return (
+            <LoadingOverlay visible={true}/>
+        )
+    }
+
+    return (
+        <ThemeProvider>
+            <Book/>
+        </ThemeProvider>
+    )
 }
