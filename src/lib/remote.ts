6b3cd3c1de96460b7dc0483b6709dbb31f74d03d
devdave <devdave@ominian.net>
2023-06-18 17:30:26 -0600

    Chang defaults type to any

--- src/lib/remote.ts
+++ src/lib/remote.ts
@@ -14,13 +14,13 @@ class APIBridge {
 
     
     
-    async info( message:string, ) {
+    async info( message:any, ) {
         
         return await this.boundary.remote("info", message, );
     }
     
     
-    async alert( message:string, ) {
+    async alert( message:any, ) {
         
         return await this.boundary.remote("alert", message, );
     }
@@ -44,13 +44,13 @@ class APIBridge {
     }
     
     
-    async fetch_chapter( chapter_id:string, ) {
+    async fetch_chapter( chapter_id:any, ) {
         
         return await this.boundary.remote("fetch_chapter", chapter_id, );
     }
     
     
-    async update_chapter( chapter_id:string, chapter_data:string, ) {
+    async update_chapter( chapter_id:any, chapter_data:any, ) {
         
         return await this.boundary.remote("update_chapter", chapter_id, chapter_data, );
     }
@@ -62,31 +62,31 @@ class APIBridge {
     }
     
     
-    async create_chapter( chapter_name:string, ) {
+    async create_chapter( chapter_name:any, ) {
         
         return await this.boundary.remote("create_chapter", chapter_name, );
     }
     
     
-    async fetch_scene( scene_uid:string, ) {
+    async fetch_scene( scene_uid:any, ) {
         
         return await this.boundary.remote("fetch_scene", scene_uid, );
     }
     
     
-    async update_scene( scene_uid:string, new_data:string, ) {
+    async update_scene( scene_uid:any, new_data:any, ) {
         
         return await this.boundary.remote("update_scene", scene_uid, new_data, );
     }
     
     
-    async create_scene( chapter_uid:string, scene_title:string, ) {
+    async create_scene( chapter_uid:any, scene_title:any, ) {
         
         return await this.boundary.remote("create_scene", chapter_uid, scene_title, );
     }
     
     
-    async save_reordered_chapters( chapters:string, ) {
+    async save_reordered_chapters( chapters:any, ) {
         
         return await this.boundary.remote("save_reordered_chapters", chapters, );
     }
